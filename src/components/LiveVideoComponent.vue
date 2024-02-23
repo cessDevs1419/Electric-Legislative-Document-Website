aa93ec471520eca4fd026662399474c00d6b6dbd
IronMan0602 <55095834+IronMan0602@users.noreply.github.com>
2024-02-23 20:00:55 +0800

    live feed working play button
    - use sample_vid.mp4 only for reference

--- src/components/LiveVideoComponent.vue
+++ src/components/LiveVideoComponent.vue
@@ -9,6 +9,27 @@ export default {
             type: String,
             default: "http://"
         }
+    },
+    data() {
+        return {
+            showButton: true
+        };
+    },
+    methods: {
+        playVideo() {
+            const video = this.$refs.videoPlayer;
+            this.showButton = false;
+            video.play();
+        },
+        handlePlay() {
+            const video = this.$refs.videoPlayer;
+            video.controls = true;
+        },
+        handlePause() {
+            const video = this.$refs.videoPlayer;
+            this.showButton = true;
+            video.controls = false;
+        }
     }
 }
 </script>
@@ -21,16 +42,18 @@ export default {
             </h6>
         </div>
         <div class="card-body dark-grey-bg border my-3 p-0 position-relative">
-            <video class="w-100 h-100" :src="videoSrc"></video>
-            <div class="button-container position-absolute top-0 w-100 h-100 d-flex align-items-center justify-content-center">
-                <button class="rounded-circle bg-dark border-0 d-flex justify-content-center align-items-center">
+            <video class="w-100 h-100" ref="videoPlayer" @play="handlePlay" @pause="handlePause" >
+                <source :src="videoSrc" type="video/mp4">
+            </video>
+            <div v-if="showButton" class="button-container position-absolute top-0 w-100 h-100 d-flex align-items-center justify-content-center">
+                <button @click="playVideo" class="rounded-circle bg-dark border-0 d-flex justify-content-center align-items-center">
                     <i class="bi bi-play-fill text-white fs-3"></i>
                 </button>
             </div>
         </div>
         <div class="card-footer px-0 border-0 bg-transparent">
-          <a class="tertiary-font fw-semibold m-0 text-decoration-none mb" :href="pageLink">View More</a>
-          <div class="underline"></div>
+            <a class="tertiary-font fw-semibold m-0 text-decoration-none mb" :href="pageLink">View More</a>
+            <div class="underline"></div>
         </div>
     </div>
 </template>

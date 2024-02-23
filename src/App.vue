59748359e59694a3f21ce548ca9a287e9dc0f5f7
IronMan0602 <55095834+IronMan0602@users.noreply.github.com>
2024-02-23 13:49:31 +0800

    hiding navbar and page header at home

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,34 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView, useRoute } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 import NavbarComponent from './components/NavbarComponent.vue';
 import PageHeaderComponent from './components/PageHeaderComponent.vue';
-import FooterComponent from './components/footer/FooterComponent.vue';
+import FooterComponent from './components/footer/FooterComponent.vue'
 
 </script>
 
+
 <template>
-  <PageHeaderComponent></PageHeaderComponent>
-  <NavbarComponent></NavbarComponent>
+  <PageHeaderComponent v-if="isHomeRoute" ></PageHeaderComponent>
+  <NavbarComponent v-if="isHomeRoute" ></NavbarComponent>
 
   <RouterView />
   <FooterComponent/>
 </template>
+
+<script>
+export default {
+  setup() {
+    const route = useRoute();
+
+    const isHomeRoute = route.path === '/';
+    
+    return {
+      isHomeRoute
+    };
+  }
+}
+</script>
 
 <style scoped>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import TemplateContainer from '@/components/TemplateContainer.vue';
 import TheWelcome from '../components/TheWelcome.vue'
+import NavbarComponent from '@/components/NavbarComponent.vue';
 </script>
 
 <template>
+
   <TemplateContainer>
     
   </TemplateContainer>

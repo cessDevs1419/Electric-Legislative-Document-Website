678cc7f5365ceaeb45df82402a93e6fb1e6c458c
IronMan0602 <55095834+IronMan0602@users.noreply.github.com>
2024-02-28 11:00:40 +0800

    working table

--- src/views/StructureViews.vue
+++ src/views/StructureViews.vue
@@ -1,14 +1,19 @@
 <script setup>
-     import HeaderContainerComponent from '@/components/HeaderContainerComponent.vue';
+    import HeaderContainerComponent from '@/components/HeaderContainerComponent.vue';
     import PDFReviewerComponent from '@/components/PDFReviewerComponent.vue';
-import TemplateContainer from '@/components/TemplateContainer.vue';
+    import TemplateContainer from '@/components/TemplateContainer.vue';
+    import SectionHeaderComponent from '@/components/SectionHeaderComponent.vue';
 </script>
 
 <template>
     <HeaderContainerComponent ></HeaderContainerComponent>
     <div class="spacer"></div>
     <TemplateContainer>
-        <PDFReviewerComponent class="pdf-reviewer mb-5"
+        <SectionHeaderComponent class="mt-5 mb-5 mt-lg-0" type="inside">
+                <template #firstWord>Organizational</template>
+                <template #secondWord>Structure</template>
+        </SectionHeaderComponent>
+        <PDFReviewerComponent class="pdf-reviewer mb-5 mt-5"
             src="https://ws-platform.s3.ap-southeast-1.amazonaws.com/328/file-example_PDF_500_kB.pdf" 
         ></PDFReviewerComponent>
     </TemplateContainer>

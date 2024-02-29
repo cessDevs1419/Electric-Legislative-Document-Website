58182f9132ab327c776a5d2e119b252ab498a0f2
IronMan0602 <55095834+IronMan0602@users.noreply.github.com>
2024-02-29 14:09:07 +0800

    calendar responsive

--- src/views/CalendarOfActivitiesViews.vue
+++ src/views/CalendarOfActivitiesViews.vue
@@ -10,41 +10,41 @@
     <HeaderContainerComponent></HeaderContainerComponent>
     <div class="spacer"></div>
     <TemplateContainer>
-        <div class="row">
+        <div class="row w-100 m-auto">
             <CalendarViewer></CalendarViewer>
             <div class="calendar-legend d-flex justify-content-center pb-4">
-              <div class="row d-flex flex-wrap">
-                <div class="col-lg-2">
+              <div class="row w-100 m-auto">
+                <div class="col-lg-2 col-md-4 col-sm-6">
                   <div class="row">
                     <div class="col-lg-3 m-0"><div class="event-color event-type-one"></div></div>
                     <div class="col-lg-5"><p>Sched01</p></div>
                   </div>
                 </div>
-                <div class="col-lg-2">
+                <div class="col-lg-2 col-md-4 col-sm-6">
                   <div class="row">
                     <div class="col-lg-3 m-0"><div class="event-color event-type-two"></div></div>
                     <div class="col-lg-5"><p>Sched02</p></div>
                   </div>
                 </div>
-                <div class="col-lg-2">
+                <div class="col-lg-2 col-md-4 col-sm-6">
                   <div class="row">
                     <div class="col-lg-3 m-0"><div class="event-color event-type-three"></div></div>
                     <div class="col-lg-5"><p>Sched03</p></div>
                   </div>
                 </div>
-                <div class="col-lg-2">
+                <div class="col-lg-2 col-md-4 col-sm-6">
                   <div class="row">
                     <div class="col-lg-3 m-0"><div class="event-color event-type-four"></div></div>
                     <div class="col-lg-5"><p>Sched04</p></div>
                   </div>
                 </div>
-                <div class="col-lg-2">
+                <div class="col-lg-2 col-md-4 col-sm-6">
                   <div class="row">
                     <div class="col-lg-3 m-0"><div class="event-color event-type-five"></div></div>
                     <div class="col-lg-5"><p>Sched05</p></div>
                   </div>
                 </div>
-                <div class="col-lg-2">
+                <div class="col-lg-2 col-md-4 col-sm-6">
                   <div class="row">
                     <div class="col-lg-3 m-0"><div class="event-color event-type-six"></div></div>
                     <div class="col-lg-5"><p>Sched06</p></div>

bca4f32d439a7521429218dfcf12eee1d6dd8938
IronMan0602 <55095834+IronMan0602@users.noreply.github.com>
2024-03-08 20:11:22 +0800

    setting specified views under municipalities
    - updating routes
    - active link

--- src/components/MunicipalitiesTemplateComponent.vue
+++ src/components/MunicipalitiesTemplateComponent.vue
@@ -1,62 +1,64 @@
 <template>
-    <div class="card-body tertiary-bg my-3 px-2 pb-5 position-relative">
-      <ul class="list-unstyled">
-        <li class="py-0 px-2" v-for="(municipality, index) in municipalities" :key="index">
-          <router-link :to="'/view-municipality/' + municipality.uuid">{{ municipality.name }}</router-link>
-          <hr class="grey-divider mt-0">
-        </li>
-      </ul>
-    </div>
+  <div class="card-body tertiary-bg my-3 px-2 pb-5 position-relative">
+    <ul class="list-unstyled px-2">
+      <li class="py-3 border grey-divider border-top-0 border-start-0 border-end-0" v-for="(municipality, index) in municipalities" :key="index">
+        <router-link class="tertiary-font fw-bold m-0" :to="'/municipalities/view-municipality/' + municipality.uuid" :exact="true">{{ municipality.name }}</router-link>
+      </li>
+    </ul>
+  </div>
 </template>
-  
-  <script>
-  import MunicipalitiesApiService from '@/services/MunicipalitiesApiService';
-  
-  export default {
-    data() {
-  return {
-    municipalities: [],
-    municipalityDetails: null,
-  };
-},
-created() {
-  const uuid = this.$route.params.uuid;
-  this.fetchData(uuid);
-},
-methods: {
-  async fetchData(uuid) {
-    try {
-      // Fetch all municipalities
-      const data = await MunicipalitiesApiService.fetch();
-      
-      // Assign the fetched data to 'municipalities' and keep the original list
-      this.municipalities = data;
-      
-      // Find the municipality with the matching UUID
-      const foundMunicipality = data.find(municipality => municipality.uuid === uuid);
-      
-      if (foundMunicipality) {
-        this.municipalityDetails = foundMunicipality;
-      } else {
-        console.error('Municipality not found for UUID:', uuid);
-      }
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
+
+<script>
+import MunicipalitiesApiService from '@/services/MunicipalitiesApiService';
+export default {
+  data() {
+    return {
+      municipalities: [],
+      municipalityDetails: null,
+    };
   },
-},
+  created() {
+    const uuid = this.$route.params.uuid;
+    this.fetchData(uuid);
+  },
+  methods: {
+    async fetchData(uuid) {
+      try {
+        // Fetch all municipalities
+        const data = await MunicipalitiesApiService.fetch();
+        
+        // Assign the fetched data to 'municipalities' and keep the original list
+        this.municipalities = data;
+        
+        // Find the municipality with the matching UUID
+        const foundMunicipality = data.find(municipality => municipality.uuid === uuid);
+        
+        if (foundMunicipality) {
+          this.municipalityDetails = foundMunicipality;
+        } else {
+          console.error('Municipality not found for UUID:', uuid);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    },
+  },
 
-  };
-  </script>
-  
-  <style scoped>
-  .card-body p:hover {
-    color: var(--secondary-color);
-    cursor: pointer;
-  }
+};
+</script>
 
-  .card-body a{
-    text-decoration: none;
-  }
-  </style>
-  
+<style scoped>
+    .card-body p:hover {
+      color: var(--secondary-color);
+      cursor: pointer;
+    }
+    
+    .card-body a{
+      text-decoration: none;
+    }
+    
+    .router-link-active{
+      color: var(--secondary-color) !important;
+      font-weight: 700;
+    }
+</style>

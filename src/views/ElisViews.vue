b313fdc930c81e5af540feae9cc33af9159e3d01
IronMan0602 <55095834+IronMan0602@users.noreply.github.com>
2024-03-13 09:37:32 +0800

    fixed responsiveness Elis Modal

--- src/views/ElisViews.vue
+++ src/views/ElisViews.vue
@@ -90,44 +90,44 @@
                     <h4 class="d-flex align-items-center fw-bold"><span class="vertical-line"></span>Document Overview</h4>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body">
+                <div class="modal-body border">
 
-                    <div class="d-flex flex-wrap align-items-start w-100 m-auto mb-4 row">
-                        <div class=" mt-0 col-md-6">
+                    <div class="d-flex flex-wrap justify-content-between align-items-start w-100 m-auto mb-4 ">
+                        <div class="mt-0 mx-0 mx-lg-0 px-2">
                             <h5 class="fw-semibold">Title</h5>
                             <p v-if="rowData.title">{{ rowData.title }}</p>
                             <p v-else>N/A</p>
                         </div>
-                        <div class=" mt-0 col-md-3">
+                        <div class="mt-0 mx-0 mx-lg-0 px-2">
                             <h5 class="fw-semibold">Type</h5>
                             <p v-if="rowData.type_name">{{ rowData.type_name }}</p>
                             <p v-else>N/A</p>
                         </div>
-                        <div class=" mt-0 col-md-3">
+                        <div class="mt-0 mx-0 mx-lg-0 px-2">
                             <h5 class="fw-semibold">Category</h5>
                             <p v-if="rowData.category_name">{{ rowData.category_name }}</p>
                             <p v-else>N/A</p>
                         </div>
                     </div>  
-                    <div class="d-flex flex-wrap align-items-start w-100 m-auto mb-4 row" >
-                        <div class=" mt-0 col-md-3">
+                    <div class="d-flex flex-wrap justify-content-between align-items-start w-100 m-auto mb-4 " >
+                        <div class="mt-0 mx-0  mx-lg-0 px-2">
                             <h5 class="fw-semibold">No</h5>
                             <p class="m-0 mb-3" v-if="rowData.no">{{ rowData.no }}</p>
                             <p class="m-0 mb-3" v-else>N/A</p>
                         </div>
-                        <div class=" mt-0 col-md-3">
+                        <div class="mt-0 mx-0  mx-lg-0 px-2">
                             <h5 class="fw-semibold">Year</h5>
                             <p class="m-0 mb-3" v-if="rowData.year">{{ rowData.year }}</p>
                             <p class="m-0 mb-3" v-else>N/A</p>
                         </div>
-                        <div class="mt-0 col-md-3">
+                        <div class="mt-0 mx-0  mx-lg-0 px-2">
                             <h5 class="fw-semibold">Bayan</h5>
                             <p class="m-0 mb-3" v-if="rowData.bayan_name">{{ rowData.bayan_name }}</p>
                             <p class="m-0 mb-3" v-else>N/A</p>
                         </div>
-                        <div class="mt-0 col-md-3" v-if="rowData.show_records_division_data">
+                        <div class="mt-0 mx-0  mx-lg-0 px-2" v-if="rowData.show_records_division_data">
                             <h5 class="fw-semibold">PDF</h5>
-                            <span class="d-flex justify-content-start text-truncate" v-if="checkIfNotNull(rowData.attachments)">
+                            <span class="attactment-container d-flex justify-content-start text-truncate overflow-x-auto" v-if="checkIfNotNull(rowData.attachments)">
 
                                 <i v-for="(items, index) in rowData.attachments" @click="openPdf(items)" :href="items" :key="index" class="cursor-pointer mx-2 bi bi-filetype-pdf fs-3">
                                 </i>    
@@ -137,37 +137,36 @@
                             </span>
                         </div>
                     </div>   
-                    <div class="d-flex flex-wrap align-items-start w-100 m-auto mb-4 row" v-if="rowData.show_legislative_division_data" >
-                        <div class=" mt-0 col-md-6">
+                    <div class="d-flex flex-wrap justify-content-between align-items-start w-100 m-auto mb-4 " v-if="rowData.show_legislative_division_data" >
+                        <div class="mt-0 mx-0  mx-lg-0 px-2">
                             <h5 class="fw-semibold">Session Type</h5>
                             <p v-if="rowData.session_type">{{ rowData.session_type }}</p>
                             <p v-else>N/A</p>
                         </div>
-                        <div class=" mt-0 col-md-3">
+                        <div class="mt-0 mx-0  mx-lg-0 px-2">
                             <h5 class="fw-semibold">Session No</h5>
                             <p v-if="rowData.session_no">{{ rowData.session_no }}</p>
                             <p v-else>N/A</p>
                         </div>
-                        <div class=" mt-0 col-md-3">
+                        <div class="mt-0 mx-0  mx-lg-0 px-2">
                             <h5 class="fw-semibold">JA/JS</h5>
                             <p v-if="rowData.ja_js">{{ rowData.ja_js }}</p>
                             <p v-else>N/A</p>
                         </div>
                     </div>    
-                    <div class="d-flex flex-wrap align-items-start w-100 m-auto mb-4 row" v-if="rowData.show_legislative_division_data" >
-                        <div class=" mt-0 col-md-6">
+                    <div class="d-flex flex-wrap justify-content-between align-items-start w-100 m-auto mb-4" v-if="rowData.show_legislative_division_data" >
+                        <div class=" mt-0 mx-0  mx-lg-0 px-2">
                             <h5 class="fw-semibold">Date of Enactment</h5>
                             <p v-if="rowData.enactment_date">{{ rowData.enactment_date || enactment_date2 }}</p>
                             <p v-else>N/A</p>
                         </div>
-                       
-                        <div class=" mt-0 col-md-3">
+                        <div class=" mt-0 mx-0  mx-lg-0 px-2">
                             <h5 class="fw-semibold">Council No.</h5>
                             <p v-if="rowData.council_no">{{ rowData.council_no }}</p>
                             <p v-else>N/A</p>
                         </div>   
                                     
-                        <div class=" mt-0 col-md-3">
+                        <div class=" mt-0 mx-0  mx-lg-0 px-2">
                             <h5 class="fw-semibold" >Author/Sponsorship</h5>
                             <p class="m-0" v-if="rowData.author">{{ rowData.author }}</p>
                             <p class="m-0" v-else>N/A</p>
@@ -195,5 +194,9 @@
         height: 40px;
         border-left: 17px solid var(--primary-color); 
         margin-right: 10px; 
+    }
+    .attactment-container{
+        max-width: 200px;
+        width: 100%;
     }
 </style>

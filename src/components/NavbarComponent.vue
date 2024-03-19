72e63e5ca07ea6f5d8d2bf26c50238774fde4cc1
IronMan0602 <55095834+IronMan0602@users.noreply.github.com>
2024-03-19 11:55:49 +0800

    updated auth user indicator
    - added user indication
    - added sidebar
    - added routing
    - added update password

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -4,7 +4,7 @@
           	<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navItems" aria-controls="navItems" aria-expanded="false" aria-label="Toggle navigation">
             	<span class="navbar-toggler-icon"></span>
           	</button>
-          	<div class="collapse navbar-collapse d-flex justify-content-between py-2" id="navItems">
+          	<div class="collapse navbar-collapse  py-2" id="navItems">
 				<ul class="navbar-nav me-auto w-100 mb-2 text-white mb-lg-0 justify-content-center ">
 					<li class="nav-item mx-1">
 						<router-link  class="nav-link text-white" to="/">Home</router-link>
@@ -69,44 +69,54 @@
 					<li class="nav-item mx-1">
 						<router-link  class="nav-link text-white" to="/contact-us">Contact Us</router-link>
 					</li>
-					<li class="nav-item dropdown-center mx-1" data-bs-theme="light"  v-if="authToken" >
-						<div class="d-flex align-items-center" class-active="active" role="button" data-bs-toggle="dropdown" aria-expanded="false"  >
-							<img class="rounded-circle h-100 w-100" src="" alt="" >
-							<a class="nav-link dropdown-toggle text-white"  >
-								Jeric Manalo
-							</a>
-						</div>
-						<ul class="dropdown-menu rounded-0">
-							<li class="d-flex align-items-center cursor-pointer">
-								<router-link class="dropdown-item my-2 d-flex align-items-center " to="/setup/profile-setup">
-									<i class="bi bi-gear me-3"></i>
-									<p class="m-0">
-										Profile Setup
-									</p>
-								</router-link>
-							</li>
-							<li class="d-flex align-items-center cursor-pointer">
-								<router-link  class="dropdown-item my-2 d-flex align-items-center " to="/setup/change-password">
-									<i class="bi bi-arrow-repeat me-3"></i>
-									<p class="m-0">
-										Password Reset
-									</p>
-								</router-link>
-							</li>
-							<li class="d-flex align-items-center cursor-pointer" @click="logout">
-								<p  class="dropdown-item my-2 d-flex align-items-center " >
-									<i class="bi bi-box-arrow-left me-3"></i>
-									<p class="m-0">
-										Logout
-									</p>
-								</p>
-							</li>
-						</ul>	
+					<li class="nav-item mx-1"   v-if="authToken" >
+						<button class="border-0 btn btn-sidebar secondary-bg d-flex justify-content-center" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+							<i class="bi bi-person-fill me-2"></i>
+							<p class="text-truncate m-0">
+								{{ name }}
+							</p>
+						</button>
 					</li>
 				</ul>
         	</div>
         </div>
     </nav>
+
+
+	<div class="offcanvas-sm-wd offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+		<div class="offcanvas-header text-center mx-auto mt-3 mb-0 pb-0 pt-5 ">
+			<h5 class="text-center" >{{ name }}</h5>
+		</div>
+		<div class="position-relative offcanvas-body h-100 mt-0 py-0 px-4 mx-2">
+			<hr class="divider">
+			<ul class="w-100 rounded-0 px-0"> 
+				<li class="d-flex align-items-center cursor-pointer">
+					<router-link class="dropdown-item my-2 d-flex align-items-center " to="/setup/profile-setup">
+						<i class="bi bi-gear me-3"></i>
+						<p class="m-0">
+							Profile Setup
+						</p>
+					</router-link>
+				</li>
+				<li class="d-flex align-items-center cursor-pointer">
+					<router-link  class="dropdown-item my-2 d-flex align-items-center " to="/setup/change-password">
+						<i class="bi bi-lock me-3"></i>
+							<p class="m-0">
+								Password Reset
+							</p>
+					</router-link>
+				</li>
+			</ul>
+      <div class="position-absolute mb-3 bottom-0 d-flex align-items-center cursor-pointer" @click="logout">
+					<p  class="dropdown-item my-2 d-flex align-items-center " >
+						<i class="bi bi-box-arrow-left me-3"></i>
+						<p class="m-0">
+							Logout
+						</p>
+					</p>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -116,7 +126,9 @@
 	export default {
 		data () {
 			return {
-				authToken: PublicUserApiService.getAuthToken()
+				authToken: PublicUserApiService.getAuthToken(),
+				user: {},
+				name: ''
 			}
 		},
 		methods: {
@@ -134,7 +146,27 @@
                     console.error('logout failed:', error);
                 }
             },
+			fetch(){
+				try{
+					PublicUserApiService.getAuthUser().then(items => {
+						this.user = items
+						this.name = this.capitalizeFirstLetter(items.full_name)
+					}).catch(error => {
+						console.log(error)
+					})
+				}catch(error){
+					console.error(error);
+				}
+			},
+			capitalizeFirstLetter(string) {
+				return string.replace(/\b\w/g, function(char) {
+					return char.toUpperCase();
+				});
+			}
 		},
+		created(){
+			this.fetch();
+		}
 
 	}
 </script>
@@ -146,5 +178,13 @@
 		color: var(--secondary-color) !important;
 		font-weight: 700;
 	}
+	.secondary-bg{
+		background-color: var(--secondary-color) !important;;
+	}
+	.btn-sidebar{
+		width: 100%;
+		max-width: 200px;
+	}
+
 
 </style>

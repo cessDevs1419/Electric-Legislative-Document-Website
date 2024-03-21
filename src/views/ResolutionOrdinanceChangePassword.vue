6b13cdcbe7ce8b5fb084848c38b65c41f7619413
IronMan0602 <55095834+IronMan0602@users.noreply.github.com>
2024-03-21 11:24:06 +0800

    fixed mobile responsiveness
    - remove excess paddings
    - fixed items orientations
    - added text wrap to long title category

--- src/views/ResolutionOrdinanceChangePassword.vue
+++ src/views/ResolutionOrdinanceChangePassword.vue
@@ -87,8 +87,8 @@
 <template>
     <HeaderContainerComponent></HeaderContainerComponent>
     <div class="spacer"></div>
-    <TemplateContainer class="d-flex align-item-center jutify-content-center mt-5 mb-5 mt-xl-0">
-        <div class="signin-container w-100 box-shadow my-5 m-auto">
+    <TemplateContainer class="d-flex align-item-center jutify-content-center mb-5 mt-xl-0">
+        <div class="signin-container w-100 box-shadow m-auto">
             <div class="row w-100 h-100 m-auto ">
                 <div class="col-lg-7 p-2 dirty-white-bg">
                     <div class="title m-auto text-center mb-5 mt-5">
